adf817998a9e0fa631aa7a85640c2b46b27a6fff
Rita Ilchenko <rita@Rita.local>
2021-06-03 18:39:16 -0400

    added options and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,25 @@
 const program = require('commander');
 program.version('0.0.1');
 
-program.description(
-`Compares two configuration files and shows a difference.`
-);
+// program.description(
+// `Compares two configuration files and shows a difference.`
+// );
 
+
+// program.parse(process.argv);
+
+//  -f, --format [type]  output format
+
+program
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  
+  // .action((filepath1, filepath2) => {
+  //   console.log('filepath1:', filepath1);
+  //   console.log('filepath2:', filepath2);
+  // });
 
 program.parse(process.argv);
 
+const options = program.opts();
