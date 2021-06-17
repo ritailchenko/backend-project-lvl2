ca2eaec61b248ab9423e38e530f47eb28b5c5172
Rita Ilchenko <rita@Rita.local>
2021-06-17 16:40:56 -0400

    returnd value to string

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
-
-import genDiff from "../src/index.js";
-
 import program from 'commander';
+import genDiff from '../src/index.js';
+
 program.version('0.0.1');
 
 program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
-  
+
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   });
 
 program.parse(process.argv);
-
--- src/index.js
+++ src/index.js
@@ -1,120 +1,87 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash'
+import _ from 'lodash';
 
 const genDiff = (file1, file2) => {
+  const path1 = path.resolve('../backend-project-lvl2', file1);
+  const path2 = path.resolve('../backend-project-lvl2', file2);
 
-  let path1 = path.resolve("..", file1);
-  let path2 = path.resolve("..", file2)
-  
-  let data1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
-  let data2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
+  const data1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
+  const data2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
 
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
-  const keys = _.union(keys1, keys2); 
+  const keys = _.union(keys1, keys2);
 
-    const result = {};
+  const result = keys.map((key) => {
+    if (!_.has(data1, key)) {
+      return {
+        key,
+        value: data2[key],
+        status: 'added',
+      };
+    }
+    if (!_.has(data2, key)) {
+      return {
+        key,
+        value: data1[key],
+        status: 'deleted',
+      };
+    }
+    if (data1[key] !== data2[key]) {
+      return {
+        key,
+        value1: data1[key],
+        value2: data2[key],
+        status: 'changed',
+      };
+    }
+    return {
+      key,
+      value: data1[key],
+      status: 'unchanged',
+    };
+  });
 
-    keys.map((key) => {
-        if (!_.has(data1, key)) {
-        //  result[key] = 'added';
-        result[key] = data2[key];
-        } 
-        else if (!_.has(data2, key)) {
-              // result[key] = 'deleted';
-              result[key] = data1[key]
-              
-            } else if (data1[key] !== data2[key]) {
-            //  result[key] = 'changed';
-              result[key] = data1[key];
-              
-              
-            } else {
-            //  result[key] = 'unchanged';
-              result[key] = data1[key]
-            }
-    })
+  const sortedResultArray = _.sortBy(result, [(o) => o.key]);
+  // console.log(sortedResultArray);
 
-    
-    return result;
-  };
- 
+  const sortedResultArrayToString = sortedResultArray.reduce((acc, currVal) => {
+    if (currVal.status === 'deleted') {
+      acc.push(`  - ${currVal.key}: ${currVal.value}`);
+    }
+    if (currVal.status === 'changed') {
+      acc.push(
+        `  - ${currVal.key}: ${currVal.value1}\n  + ${currVal.key}: ${currVal.value2}`
+      );
+    }
+    if (currVal.status === 'added') {
+      acc.push(`  + ${currVal.key}: ${currVal.value}`);
+    }
+    if (currVal.status === 'unchanged') {
+      acc.push(`    ${currVal.key}: ${currVal.value}`);
+    }
+    return acc;
+  }, []);
+  // console.log(`{\n${sortedResultArrayToString.join('\n')} \n}`);
 
+  return `{\n${sortedResultArrayToString.join('\n')} \n}`;
+  // sortedResultArray.forEach((obj) => {
+  //   if (obj.status === 'deleted') {
+  //     console.log(`{ - ${obj.key}: ${obj.value}`);
+  //   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const genDiff = (file1, file2) => {
-//   let path1 = path.resolve("..", file1);
-//   let path2 = path.resolve("..", file2)
-  
-//   const obj1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
-//   const obj2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
-
-//   let key1 = Object.keys(obj1);
-//   let key2 = Object.keys(obj2);
-//   let value1 = Object.values(obj1);
-//   let value2 = Object.values(obj2);
-
-
-//   let emptyObj = {};
-
-//   // only in 1st obj
-//   for (let i of key1) {
-//     if (!key2.includes(i)) {
-//       emptyObj[`- ${i}`] = obj1[i]
-//       // emptyObj[i] = obj1[i];
-//     }
-//   }
-
-//   // same key and value in booth
-//   for (let i of value1) {
-//     if (value2.includes(i)) {
-//       emptyObj[key1[value1.indexOf(i)]] = value1[value1.indexOf(i)];
-//     }
-//   }
-
-//   // same key in booth, but different value
-//   for (let i of key1) {
-//     if (key2.includes(i)) {
-//       if (obj1[i] !== obj2[i]) {
-//         emptyObj[`- ${i}`] = obj1[i];
-//         emptyObj[`+ ${i + 1}`] = obj2[i]
-//       }
-//     }
-//   }
-
-//   // only in 2nd obj
-//   for (let i of key2) {
-//     if (!key1.includes(i)) {
-//       emptyObj[`+ ${i}`] = obj2[i];
-//     }
-//   }
-//     return emptyObj;
-// };
+  //   if (obj.status === 'changed') {
+  //     return ` - ${obj.key}: ${obj.value1}\n + ${obj.key}: ${obj.value2}`;
+  //   }
+  //   if (obj.status === 'added') {
+  //     return ` + ${obj.key}: ${obj.value}`;
+  //   }
+  //   if (obj.status === 'unchanged') {
+  //     return `   ${obj.key}: ${obj.value}`;
+  //   }
+  // });
+};
 
 // console.log(genDiff(file1, file2));
 
