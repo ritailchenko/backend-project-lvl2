6b3b4ad11662c9235731bfe2d726fb19f4907469
Rita Ilchenko <rita@Rita.local>
2022-03-18 22:48:06 +0600

    first draft of stylish function

--- test2.js
+++ test2.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
-const stringify = (obj, symb = ' ', spaceCount) => {
+const stringify = (obj, symb = '  ', spaceCount = 1) => {
   const iter = (currentValue, depth) => {
     if (!_.isObject(currentValue)) {
       // если не объект отдаем значение
-      return currentValue.toString();
+      return currentValue;
     }
     const indentSize = depth * spaceCount; // считаем кол-во отступов
     const currentIndent = symb.repeat(indentSize); // заполняем отступы нужным символом
@@ -13,7 +13,7 @@ const stringify = (obj, symb = ' ', spaceCount) => {
     // проходим по значения объект
     const lines = _.map(
       currentValue,
-      (value, key) => `${currentIndent}${key}: ${iter(value, depth + 1)}`,
+      (value, key) => `${currentIndent}${key}: ${iter(value, depth + 2)}`,
     );
     // отдаем результат
     return ['{', ...lines, `${bracketIndent}}`].join('\n');
@@ -21,7 +21,7 @@ const stringify = (obj, symb = ' ', spaceCount) => {
 
   return iter(obj, 1);
 };
-
+// console.log(stringify(obj3));
 const obj1 = {
   common: {
     setting1: 'Value 1',
@@ -46,82 +46,87 @@ const obj2 = {
       },
     },
   },
+  setting2: 'apple',
   setting5: {
     key5: 'value5',
   },
-  setting2: 'apple',
 };
 
 const buildTree = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
 
   const resultTree = keys.map((key) => {
-    // console.log(data1);
-    if (!_.has(data1, key)) {
-      // console.log();
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       return {
-        key: `+ ${key}`,
+        key,
+        children: buildTree(data1[key], data2[key]),
+        status: 'nested',
+      };
+    }
+
+    if (!_.has(data1, key)) {
+      return {
+        key,
         value: data2[key],
         status: 'added',
       };
     }
     if (!_.has(data2, key)) {
       return {
-        key: `- ${key}`,
+        key,
         value: data1[key],
         status: 'deleted',
       };
     }
-    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return {
-        key: `  ${key}`,
 
-        children: buildTree(data1[key], data2[key]),
-        status: 'nested',
-      };
-    }
     if (data1[key] !== data2[key]) {
       return {
-        key: `  ${key}`,
+        key,
         value1: data1[key],
         value2: data2[key],
         status: 'changed',
       };
     }
     return {
-      key: `  ${key}`,
+      key,
       value: data1[key],
       status: 'unchanged',
     };
   });
   const sortedResultArray = _.sortBy(resultTree, [(o) => o.key]);
-  console.log(sortedResultArray);
-
-  const sortedResultArrayToString = sortedResultArray.reduce((acc, currVal) => {
-    if (currVal.status === 'deleted') {
-      acc[currVal.key] = currVal.value;
-    }
-    if (currVal.status === 'changed') {
-      // acc.push(
-      //   `  - ${currVal.key}: ${currVal.value1}\n  + ${currVal.key}: ${currVal.value2}`,
-      // );
-      acc[currVal.key] = `-${currVal.value1}\n            +${currVal.value2}`;
-    }
-    if (currVal.status === 'added') {
-      // acc.push(`  + ${currVal.key}: ${currVal.value}`);
-      acc[currVal.key] = currVal.value;
-    }
-    if (currVal.status === 'unchanged') {
-      acc[currVal.key] = currVal.value;
-    }
-    if (currVal.status === 'nested') {
-      acc[currVal.key] = currVal.children;
-    }
-
-    return acc;
-  }, {});
-
-  return `{\n${sortedResultArrayToString.join('\n')}\n}`;
+  return sortedResultArray;
+  // return `{\n${sortedResultArrayToString.join('\n')}\n}`;
 };
 
-buildTree(obj1, obj2);
+const stylish = (arr) => {
+  const styledTree = arr.reduce((acc, obj) => {
+    switch (obj.status) {
+      case 'added':
+        acc[`+ ${obj.key}`] = obj.value;
+
+        break;
+      case 'deleted':
+        acc[`- ${obj.key}`] = obj.value;
+        break;
+      case 'unchanged':
+        acc[`  ${obj.key}`] = obj.value;
+        break;
+      case 'changed':
+        acc[`- ${obj.key}`] = `${obj.value2}`;
+        acc[`+ ${obj.key}`] = `${obj.value1}`;
+
+        break;
+      case 'nested':
+        acc[`  ${obj.key}`] = stylish(obj.children);
+
+        break;
+      default:
+        return true;
+    }
+    return acc;
+  }, {});
+  return styledTree;
+};
+
+// stylish(buildTree(obj1, obj2));
+console.log(stringify(stylish(buildTree(obj1, obj2))));
