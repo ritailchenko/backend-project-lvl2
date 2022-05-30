a727e8315bc9d9f78cdf053354bd2d9b36ff5f11
Rita Ilchenko <rita@Rita.local>
2022-05-29 21:00:43 -0400

    fixed test file

--- __tests__/genDiffModuleCheck.test.js
+++ __tests__/genDiffModuleCheck.test.js
@@ -17,32 +17,24 @@ const stringForPlain = readFile('testStringForPlain.txt').trim();
 const stringForJson = readFile('testStringForJson.txt').trim();
 
 test('file1.json compare to file2.json to equal testStringForStylish', () => {
-  expect(genDiff('file1.json', 'file2.json', { format: 'stylish' })).toMatch(
+  expect(genDiff('file1.json', 'file2.json', 'stylish')).toMatch(
     stringForStylish,
   );
 });
 test('file1.yaml compare to file2.yaml to equal testStringForStylish', () => {
-  expect(genDiff('file1.yaml', 'file2.yaml', { format: 'stylish' })).toMatch(
+  expect(genDiff('file1.yaml', 'file2.yaml', 'stylish')).toMatch(
     stringForStylish,
   );
 });
 test('file1.json compare to file2.json to equal testStringForPlain', () => {
-  expect(genDiff('file1.json', 'file2.json', { format: 'plain' })).toMatch(
-    stringForPlain,
-  );
+  expect(genDiff('file1.json', 'file2.json', 'plain')).toMatch(stringForPlain);
 });
 test('file1.yaml compare to file2.yaml to equal testStringForPlain', () => {
-  expect(genDiff('file1.yaml', 'file2.yaml', { format: 'plain' })).toMatch(
-    stringForPlain,
-  );
+  expect(genDiff('file1.yaml', 'file2.yaml', 'plain')).toMatch(stringForPlain);
 });
 test('file1.json compare to file2.json to equal testStringForJson', () => {
-  expect(genDiff('file1.json', 'file2.json', { format: 'json' })).toMatch(
-    stringForJson,
-  );
+  expect(genDiff('file1.json', 'file2.json', 'json')).toMatch(stringForJson);
 });
 test('file1.yaml compare to file2.yaml to equal testStringForJson', () => {
-  expect(genDiff('file1.yaml', 'file2.yaml', { format: 'json' })).toMatch(
-    stringForJson,
-  );
+  expect(genDiff('file1.yaml', 'file2.yaml', 'json')).toMatch(stringForJson);
 });
